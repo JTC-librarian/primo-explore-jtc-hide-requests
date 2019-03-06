3b4525d91fdfd14d2584acb2d9cf9b510d9f25ec
JTC-librarian <jamesclark545@gmail.com>
2019-03-06 17:11:16 +0000

    updated

--- js/hide-requests.js
+++ js/hide-requests.js
@@ -16,17 +16,5 @@ function hideILLRequests() {
 		request_div.style.display = "none";
 	}
 }
-
-function hideHTGIRequests() {
-	var request_span_array = document.querySelectorAll('span[translate="nui.request.signin_additional"]');
-	for (i=0; i<request_span_array.length; i++) {
-		request_div1 = request_span_array[0].parentNode;
-		request_div2 = request_div1.parentNode;
-		request_prm_alert_bar = request_div2.parentNode;
-		request_prm_alert_bar.style.display = "none";
-	}
-}
 setInterval(hideIssueRequests, 2000);
 setInterval(hideILLRequests, 2000);
-setInterval(hideHTGIRequests, 2000);
-window.alert("hide");
